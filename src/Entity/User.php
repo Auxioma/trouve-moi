b697fbd5f784562c7deb9ad3513167a6cd557a00
Auxioma <auxioma@yandex.fr>
2026-04-17 13:12:54 +0200

    user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -33,6 +33,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 use Vich\UploaderBundle\Mapping\Attribute as Vich;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: 'user')]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 #[UniqueEntity(fields: ['siren'], message: 'There is already an account with this siren')]
